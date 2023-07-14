965c20e5b88410384e098f8d8662d29450941b08
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2023-07-14 20:08:34 -0300

    update proyect testing area

---
+++ sitepro/1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : ""); ?></title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta name="keywords" content="<?php echo htmlspecialchars(($seoKeywords !== "") ? $seoKeywords : ""); ?>" />
+		<!-- Facebook Open Graph -->
+	<meta property="og:title" content="<?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : ""); ?>" />
+	<meta property="og:description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta property="og:image" content="<?php echo htmlspecialchars(($seoImage !== "") ? "{{base_url}}".$seoImage : ""); ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20200821171113" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20200820115941" rel="stylesheet" type="text/css" id="wb-site-stylesheet" />
+	<link href="css/common.css?ts=1598062026" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1598062026" rel="stylesheet" type="text/css" id="wb-page-stylesheet" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body class="site <?php if (isset($wbPopupMode) && $wbPopupMode) echo ' popup-mode'; ?> " <?php if (isset($wbLandingPage) && $wbLandingPage) echo ' data-landing-page="'.$wbLandingPage.'"'; ?>><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance1" class="wb_element wb_text_element" style=" line-height: normal;"><h4 class="wb-stl-custom8" style="text-align: center;">Asociación de Empresarios Gallegos de Argentina</h4>
+
+<p style="text-align: center;"> </p>
+
+<h4 class="wb-stl-custom8" style="text-align: center;">(AEGA)</h4>
+</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 155px;"><div id="wb_element_instance0" class="wb_element wb_element_picture" title=""><a href="https://www.wnpower.com/constructor-web/" target="_blank" rel="nofollow"><img alt="gallery/8d1990a18e053dfb546b1584c592ca4d.lock" src="gallery_gen/f22aa653caf21eee0a5e7e8f8862a4e3.png"></a></div><div id="wb_element_instance2" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
